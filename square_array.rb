76693a6efefaddd3cbad83af21fb0b3ce80b6b01
Marcjanna Mietla <mietla.m@gmail.com>
2020-05-04 15:10:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+
+numbers.each do |number|
+puts "#{number * number}"
+
+
 end
+
+end
+numbers =[1,2,3]
+square_array(numbers)
+
+numbers.collect {|number| number * number}
+square_array(numbers)
